b55dd6a7f457e6777b7840b3359dd07ca3f4aabb
russellfunk <russellfunk@gmail.com>
2017-07-11 13:45:59 -0500

    Add more documentation

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.napoleon'
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
