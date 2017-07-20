964245039b3fd83370db37773012350257b4250a
russellfunk <russellfunk@gmail.com>
2017-07-20 08:11:35 -0500

    Bug fixes

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ __copyright__ = "Copyright (C) 2017"
 from setuptools import setup, Extension, find_packages
 
 setup(name="cdindex",
-    version="1.0.8",
+    version="1.0.9",
     description="Package for computing the cdindex.",
     author="Russell J. Funk",
     author_email="russellfunk@gmail.com",
